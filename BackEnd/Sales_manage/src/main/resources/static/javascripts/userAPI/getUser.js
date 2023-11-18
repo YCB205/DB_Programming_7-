a7bd771c266eabca3352a92db32df22a512f48fc
YCB205 <ckd5310@naver.com>
2023-11-18 18:31:27 +0900

    로그인 기타 수정

--- BackEnd/Sales_manage/src/main/resources/static/javascripts/userAPI/getUser.js
+++ BackEnd/Sales_manage/src/main/resources/static/javascripts/userAPI/getUser.js
@@ -3,15 +3,22 @@ const apiUrl = '/user';
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        // Assuming you have elements with corresponding IDs in your HTML
-        document.getElementById('brandName').textContent = data.brandName;
-        document.getElementById('officeName').textContent = data.officeName;
-        document.getElementById('position').textContent = data.position;
-        document.getElementById('name').textContent = data.name;
-        document.getElementById('email').textContent = data.email;
-        document.getElementById('idStoreManager').textContent = data.idStoreManager;
-        document.getElementById('number').textContent = data.number;
-
+        if (data.position==="점주"){
+            document.getElementById('brandName').textContent = data.brandName;
+            document.getElementById('officeName').textContent = data.officeName;
+            document.getElementById('position').textContent = data.position;
+            document.getElementById('name').textContent = data.name;
+            document.getElementById('email').textContent = data.email;
+            document.getElementById('idStoreManager').textContent = data.idStoreManager;
+            document.getElementById('number').textContent = data.number;
+        } else if(data.position==="매니저"){
+            document.getElementById('brandName').textContent = data.brandName;
+            document.getElementById('position').textContent = data.position;
+            document.getElementById('name').textContent = data.name;
+            document.getElementById('email').textContent = data.email;
+            document.getElementById('idStoreManager').textContent = data.idManager;
+            document.getElementById('number').textContent = data.phoneNumber;
+        } else {return null;}
 
     })
     .catch(error => console.error('Error:', error));
