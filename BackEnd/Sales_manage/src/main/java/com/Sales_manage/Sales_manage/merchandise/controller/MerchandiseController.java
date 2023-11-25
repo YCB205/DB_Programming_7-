d0ad035cdf0c921bdbdef36ae2b05076b0377ff7
최길한 <69311223+brook12504@users.noreply.github.com>
2023-11-25 19:56:58 +0900

    주문서등록 상품조회 백엔드
    처음 페이지에 들어갈 때 모든 상품 조회
    카테고리 누를 때 해당 카테고리에 의한 상품 조회
    검색버튼 누를 때 해당 상품명에 해당하는 상품 조회

---
+++ BackEnd/Sales_manage/src/main/java/com/Sales_manage/Sales_manage/merchandise/controller/MerchandiseController.java
@@ -0,0 +1,33 @@
+package com.Sales_manage.Sales_manage.merchandise.controller;
+
+
+import com.Sales_manage.Sales_manage.merchandise.service.MerchandiseService;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class MerchandiseController {
+
+    @Autowired
+    private MerchandiseService merchandiseService;
+
+    @GetMapping("/product")
+    @ResponseBody
+    public Map<String, List<Map<String, Object>>> getProductsOrder(@RequestParam(value = "product_name", defaultValue = "") String productName,
+                                                              @RequestParam(value = "category") List<String> categories,
+                                                              HttpSession session)
+    {
+        String loggedInUserId = (String) session.getAttribute("loggedInUserId");
+        return merchandiseService.getProductsOrder(productName, categories, loggedInUserId);
+    }
+
+
+
+}
