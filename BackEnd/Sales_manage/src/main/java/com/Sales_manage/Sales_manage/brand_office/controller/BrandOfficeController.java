8313f4e108e36ffd21192eb87f3792cf32c38a1f
YCB205 <ckd5310@naver.com>
2023-11-28 21:10:39 +0900

    get /branch-storeManagers 세션 제한 추가

--- BackEnd/Sales_manage/src/main/java/com/Sales_manage/Sales_manage/brand_office/controller/BrandOfficeController.java
+++ BackEnd/Sales_manage/src/main/java/com/Sales_manage/Sales_manage/brand_office/controller/BrandOfficeController.java
@@ -3,7 +3,9 @@ package com.Sales_manage.Sales_manage.brand_office.controller;
 import com.Sales_manage.Sales_manage.brand_office.service.BrandOfficeService;
 import com.Sales_manage.Sales_manage.brand_office.dto.BrandOfficeDTO;
 import com.Sales_manage.Sales_manage.store_manager.entity.StoreManagerEntity;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,10 +27,15 @@ public class BrandOfficeController {
     @ResponseBody
     public List<List<Object>> getBranchStoreManagers(
             @RequestParam(name = "officeName", required = false) String officeName,
-            @RequestParam(name = "name", required = false) String name) {
-        // 서비스 레이어를 통해 데이터 조회
-        List<List<Object>> result = brandOfficeService.findBranchStoreManagers(officeName, name);
-        return result;
+            @RequestParam(name = "name", required = false) String name, HttpSession session) {
+        String loggedInUserRole = (String) session.getAttribute("loggedInUserRole");
+        if ("manager".equals(loggedInUserRole)) {
+            List<List<Object>> result = brandOfficeService.findBranchStoreManagers(officeName, name);
+            return result;
+        } else {
+            ResponseEntity.badRequest().build();
+        }
+        return null;
     }
 
 }
