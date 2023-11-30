e6b8d1645a0615ba686c2104e80598c00648feec
Dongju20 <114079607+Dongju20@users.noreply.github.com>
2023-11-30 19:05:51 +0900

    매출조회 구현중

--- BackEnd/Sales_manage/src/main/resources/static/javascripts/SalesManager/SaleManager.js
+++ BackEnd/Sales_manage/src/main/resources/static/javascripts/SalesManager/SaleManager.js
@@ -4,10 +4,15 @@ window.onload = function () {
 
 //매출 정보 조히 차트 띄우기
 function getOrderManager(){
-
+    getFetch();
 }
 
 //fetch함수 부르기
 function getFetch(){
-    const a = 1;
+    const url = `/sales?product=all&start_date=date&end_date=date`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data=>{
+            console.log(data);
+        })
 }
