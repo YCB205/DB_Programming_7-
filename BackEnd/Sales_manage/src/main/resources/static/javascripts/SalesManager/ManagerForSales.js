559d4fa70fb7b0b70e041123858b9836083b1f7d
Dongju20 <114079607+Dongju20@users.noreply.github.com>
2023-12-12 13:01:00 +0900

    .

--- BackEnd/Sales_manage/src/main/resources/static/javascripts/SalesManager/ManagerForSales.js
+++ BackEnd/Sales_manage/src/main/resources/static/javascripts/SalesManager/ManagerForSales.js
@@ -70,7 +70,6 @@ function addBtnRadio(data){
             btngroup.appendChild(input);
             btngroup.appendChild(label);
         }
-
 }
 
 function checkTimeValid(){
@@ -97,7 +96,7 @@ function getTime(){
 //fetch부르기
 function getFetch(startdate, enddate){
     const product =[];
-    const company =['1001'];
+    const company =[];
     const startDate = convertTimeType(startdate);
     const endDate = convertTimeType(enddate);
     const getRadio = document.getElementById('optionsRadios2');
@@ -114,6 +113,7 @@ function getFetch(startdate, enddate){
     fetch(url)
         .then(response => response.json())
         .then(data=>{
+            console.log(data);
             addTableRow(data);
         })
     fetch(url2)
@@ -137,4 +137,95 @@ function convertTimeType(inputDateTime) {
     const formattedTime = `${hours}:${minutes}:${seconds}`;
     console.log((`${formattedDate} ${formattedTime}`));
     return ((`${formattedDate} ${formattedTime}`));
+}
+let myMap = new Map();
+
+let labels = [];
+let salesData = [];
+let profitsData = [];
+
+function groupAndConvertToJSON(data) {
+    const groupedData = {};
+
+    // 데이터를 그룹화합니다.
+    data.forEach(item => {
+        const key = item[0]; // [0]번째 숫자 값을 그룹화 키로 사용합니다.
+
+        if (!groupedData[key]) {
+            groupedData[key] = [];
+        }
+
+        // 데이터를 JSON 객체로 변환하여 그룹에 추가합니다.
+        groupedData[key].push({
+            jumju: item[1],
+            category : item[2],
+            id: item[3],
+            name: item[4],
+            profit: item[6],
+            sales: item[7],
+        });
+    });
+
+    // 그룹화된 데이터를 배열로 변환합니다.
+    const result = Object.values(groupedData);
+
+    return result;
+}
+
+function addTableRow(data){
+    const tbody = document.getElementById('tbody');
+    //데이터 전처리하기
+    const groupedAndConvertedData = groupAndConvertToJSON(data);
+    console.log(groupedAndConvertedData);
+    myMap.clear();
+    labels.length = 0;
+    salesData.length = 0;
+    profitsData.length = 0;
+    //자식요소 삭제하기
+    while (tbody.firstChild) {
+        console.log("삭제합니다..");
+        tbody.removeChild(tbody.firstChild);
+    }
+    let tobdy = document.getElementById('tbody');
+    for(let index = 0; index < groupedAndConvertedData.length - 1; index++) {
+        const ordersheet = groupedAndConvertedData[index];
+        let trClass = document.createElement('tr');
+        let th = document.createElement('th');
+        th.setAttribute('rowspan',ordersheet.length+1);
+        th.setAttribute('scope','row');
+        th.setAttribute('style','vertical-align: middle');
+        th.textContent = ordersheet[0].jumju;
+        trClass.appendChild(th);
+        tbody.appendChild(trClass);
+        myMap.set(ordersheet[0].jumju,0);
+        for(let jdex =0; jdex < ordersheet.length; jdex++) {
+            let data = ordersheet[jdex];
+            console.log(data);
+            let tr = document.createElement('tr');
+            let td1 = document.createElement('td');
+            let td2 = document.createElement('td');
+            let td3 = document.createElement('td');
+            let td4 = document.createElement('td');
+            let td5 = document.createElement('td');
+            console.log(data[1]);
+            td1.textContent = data.category;
+            td2.textContent = data.name;
+            td3.textContent = data.id;
+            td4.textContent = data.profit;
+            td5.textContent = data.sales;
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            tbody.appendChild(tr);
+        }
+    }
+    // myMap.set(total[0],[total[1],total[2]]);
+    // drawBarChart();
+    // drawDoughnut();
+    // drawDoughnut2();
+    // const showChart = document.getElementById('showChart');
+    // showChart.style.display = 'none';
+    // index = 0;
 }
